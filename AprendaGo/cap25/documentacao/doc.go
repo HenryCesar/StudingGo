8ff9a95ad65080699bd136966add6b5818a36c17
Henrique C. P. S. Almeida <henrique.almeida@gruposplice.com.br>
2021-06-01 09:47:08 -0300

    feat(cap25): learning about how to document a script/code in golang

---
+++ AprendaGo/cap25/documentacao/doc.go
@@ -0,0 +1,6 @@
+/*
+	Package escrevendo ilustra como escrever uma documentação.
+	Não tem utilidade nenhuma, é apenas para demonstração.
+	O último comentário, eu vou ignorar.
+*/
+package escrevendo
