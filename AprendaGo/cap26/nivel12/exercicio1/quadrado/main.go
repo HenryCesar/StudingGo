38fb85d1d148278d0df1832f6d04b9f9b6b0a6db
Henrique C. P. S. Almeida <henrique.almeida@gruposplice.com.br>
2021-06-10 12:00:24 -0300

    fix(cap26/nivel12): exercicio1 was fixed

--- AprendaGo/cap26/nivel12/exercicio1/quadrado/main.go
+++ AprendaGo/cap26/nivel12/exercicio1/quadrado/main.go
@@ -1,6 +1,7 @@
 // Package quadrado deixa disponível facilmente recebe o "quadrado" do número.
 package quadrado
 
+// UtilizandoQuadrado recebe um valor inteiro e retorna o quadrado deste valor.
 func UtilizandoQuadrado(x int) int {
 	return x * x
 }
